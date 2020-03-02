85f552792a52a1f047a7aba6c7963b3f6f0f4e63
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-03-02 16:20:30 -0700

    1.0

---
+++ First-Example/start.js
@@ -0,0 +1,141 @@
+// console.log("Hello World!");
+// var yourName = prompt("What's your name?");
+// console.log(yourName);      // browser console specific
+
+// var largerNumber = 13;
+// var smallerNumber = 5;
+
+// largerNumber++;			// adds one to the number
+
+// console.log(largerNumber++)	// prints out the var
+
+// console.log(++largerNumber)	// adds one to the number
+
+// var smallNumber = 2;
+// var sameSmallNumber = 2;
+
+// // console.log(largerNumber);
+
+// var smallNumber = 2;
+// var string = Number
+
+// var john = {
+//     value1: 1,
+//     value2: 332,
+//     string1: "454646546456546546asdadadada"
+// }
+
+// var afterNumberTransformation = Number(john);
+// console.log(afterNumberTransformation)
+
+// var userinput = "1337";
+// var valueIwantToValidaeExists = 1337;
+
+// if (userinput == valueIwantToValidaeExists) {
+//     console.log("VALID");
+// } else {
+//     console.log("Invalid");
+// }
+
+// var a = 3;
+// var b = 6;
+// var c = 9;
+
+// if (a < b || b > c) {
+//     console.log("Got a result")
+// } else {
+//     console.log("didn't get a result a result")
+// }
+
+// var e = true;
+// console.log(e);     // comes out true
+
+// var e = !true;
+// console.log(e);     // comes out false
+
+// var e = !(!true);
+// console.log(e);     // comes out true
+
+// var e = !(!(!true));
+// console.log(e);     // comes out true
+
+// b = (6 > 7) && ("a" === "Hello");    // Statements
+
+// b = false || (6 == "6");  // true regardless of the data type
+// console.log(b)
+
+// b = false || (6 === "6"); // false as its checking for data types
+// console.log(b)
+
+// b = false || (6 == "-6"); // false
+// console.log(b)
+
+// b = null
+// console.log(b)  // always viewed as false. like NaN
+
+// while loop example 
+
+// var interator = 0;
+// var score = 0;
+// while (true) {
+//     score = interator;
+//     console.log("your score is : " + score);
+//     interator++;
+// }
+
+for(var i=0; i<101; i++) {
+    if((i%5 == 0) && (i%3 == 0)) {
+       console.log("FizzBuzz");
+    } else if (i%5==0) {
+        console.log("Buzz");
+    } else if (i%3 == 0){
+        console.log("Fizz");    //
+    } else {
+        console.log(i);     // prints out the other numbers
+    }
+   }
+
+var arrayOne =[1,2,3,4,5]
+console.log(arrayOne);          // prints out the list in the array
+
+var shoppingList =["coffee","peppers","apple juice"];
+var itemTwo = shoppingList[i];
+
+console.log(itemTwo);                   // prints out the list
+shoppingList.push("oranges")            // adds oranges at the end
+
+shoppingList[1]="not boring non-peppers";   // prints the second index
+console.log(itemTwo);
+console.log(shoppingList);
+
+shoppingList[9001]="shorter";           // 9001 is the last index position
+console.log(shoppingList);
+console.log(shoppingList[10]);      // trys to print out on the 10th index
+
+var fruits =["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2,2   , "Lemon" , "Kiwi") // index position, how many you're adding
+console.log(fruits);                // prints the additions
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+for (var i = 0; i <= fruits.length; i++){       // prints out the list of items in the array
+    console.log(fruits[i]);         
+}
+
+var array =[];
+for(var i = 0; i<13; i++){      // prints out 13 indexes aka 0 - 12
+    array.push(i);
+}
+
+console.log(array);     // prints out the request above
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var computerStuff = ["computer", "hose", "cable", "idk", "fans"];
+
+for (var i = 0; i < fruits.length; i++){       // prints out the list of items in the array
+ 
+    var placeHolder = computerStuff[i];
+    computerStuff[i] = fruits[i];
+    fruits[i] = placeHolder;
+}
+console.log(computerStuff);
+console.log(fruits);
